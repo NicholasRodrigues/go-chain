7928e71922bc99a92cab242c14cb90dbbf8eb77e
NicholasRodrigues <nicholasgabriel65@gmail.com>
2024-05-20 19:53:24 -0300

    feat: creating proof of work

--- internal/blockchain/proof_of_work.go
+++ internal/blockchain/proof_of_work.go
@@ -4,7 +4,18 @@ import (
 	"math/big"
 )
 
+const Difficulty = 16
+
 type ProofOfWork struct {
 	block *Block
 	T     big.Int
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-Difficulty))
+
+	pow := &ProofOfWork{b, *target}
+
+	return pow
 }
