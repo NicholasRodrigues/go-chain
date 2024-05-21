b1a2c9a8dd741810645fb91b5c26af31c391172c
gabrielfnayres <gabn2012@gmail.com>
2024-05-21 08:52:55 -0300

    setting the Block and Blockchain structure also with a simple hash function implemented

--- internal/blockchain/block.go
+++ internal/blockchain/block.go
@@ -1,1 +1,22 @@
-package blockchain
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"strconv"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte // Transactions
+	prevBlockHash []byte
+	Hash          []byte
+	Counter       uint64 // Nonce
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.prevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+	b.Hash = hash[:]
+}
