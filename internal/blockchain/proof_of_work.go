07544866ceb99b710b0f1c83bd9d0d4b19429f79
gabrielfnayres <gabn2012@gmail.com>
2024-05-22 18:34:31 -0300

    proof of work function implemented (we now have to test it)

--- internal/blockchain/proof_of_work.go
+++ internal/blockchain/proof_of_work.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
+	"math"
 	"math/big"
 	"strconv"
 )
 
 const targetBits = 24
+
+var maxNonce = math.MaxInt64
 
 type ProofOfWork struct {
 	block *Block
@@ -28,4 +32,33 @@ func (pow *ProofOfWork) prepareData(nonce int) []byte {
 	)
 
 	return data
+}
+
+func (pow *ProofOfWork) RunProofOfWork() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	if pow.block == nil {
+		pow.block.Data = pow.block.GenesisBlock().Data
+	} else {
+		temp_block := pow.block
+		temp_block.SetHash()
+		pow.block = temp_block
+	}
+
+	for nonce < maxNonce {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+
+		hashInt.SetBytes(hash[:])
+		if hashInt.Cmp(&pow.T) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+
+	return nonce, hash[:]
+
 }
