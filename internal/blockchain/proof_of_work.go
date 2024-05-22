fa031f87385e040aa6ed693516fd80c9ddff9647
gabrielfnayres <gabn2012@gmail.com>
2024-05-22 17:31:10 -0300

    setup hash function to proof of work

--- internal/blockchain/proof_of_work.go
+++ internal/blockchain/proof_of_work.go
@@ -1,10 +1,31 @@
 package main
 
 import (
+	"bytes"
 	"math/big"
+	"strconv"
 )
+
+const targetBits = 24
 
 type ProofOfWork struct {
 	block *Block
 	T     big.Int
+}
+
+// Prepare pow data, this function hashes the data and returns it
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.prevBlockHash,
+			pow.block.Data,
+			[]byte(strconv.FormatInt(pow.block.Timestamp, 10)),
+			[]byte(strconv.FormatInt(targetBits, 10)),
+			[]byte(strconv.FormatInt(int64(nonce), 10)),
+		},
+		[]byte{},
+	)
+
+	return data
 }
